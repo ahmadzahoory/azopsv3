4cab73dc764cb3d2a5d713a03b6980601a216f29
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2024-03-26 00:18:10 +0530

    Create lab-mon-script.sh

---
+++ lab-mon-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y apache2 wget unzip
+cd /var/www/html/
+sudo rm index.html
+sudo wget https://raw.githubusercontent.com/ahmadzahoory/azadminv2/master/lab-mon-code-l.zip
+sudo unzip lab-mon-code-l.zip
+sudo systemctl restart apache2.service
+#End
