485de4f77cfd534b75b166a8257ccc10cdd33a07
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2024-03-26 00:41:36 +0530

    Update lab-mon-script.sh

--- lab-mon-script.sh
+++ lab-mon-script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y apache2 wget unzip
+sudo apt install -y apache2
+sudo apt install -y wget
+sudo apt install -y unzip
 cd /var/www/html/
 sudo rm index.html
 sudo wget https://raw.githubusercontent.com/ahmadzahoory/azopsv3/master/lab-mon-code-l.zip
