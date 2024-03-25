168634170aaf33523ace55f7f4ee5220bed00696
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2024-03-26 00:08:48 +0530

    Create lab-lb_script.sh

---
+++ lab-lb_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get install -y apache2 wget unzip
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get -y update
+sudo apt-get install -y php8.2
+sudo apt-get install -y stress-ng
+cd /var/www/html/
+sudo rm index.html
+sudo wget https://raw.githubusercontent.com/ahmadzahoory/azadminv2/master/lab-lb-code.zip
+sudo unzip lab-lb-code.zip
+sudo systemctl restart apache2.service
